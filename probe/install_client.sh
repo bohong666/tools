afc07893888382653f8820b7f5de805076b61ea5
bohong666 <86768133+bohong666@users.noreply.github.com>
2025-12-25 13:49:16 +0800

    Update install_client.sh

--- probe/install_client.sh
+++ probe/install_client.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-VERSION="2025.12.25-002"
+VERSION="2025.12.25-003"
 echo "===> Installing Monitor Client, version: $VERSION"
 
-SERVER="http://YOUR_SERVER_IP:6666"
+SERVER="http://YOUR_SERVER_IP"
 NAME=$(hostname)
 
 cat > /usr/local/bin/monitor-client.sh <<EOF
